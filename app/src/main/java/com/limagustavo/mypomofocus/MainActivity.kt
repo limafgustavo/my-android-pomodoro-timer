2a5625beda0734e4f2713fe9e53d63a46ba423d4
Gustavo Lima <136847064+limafgustavo@users.noreply.github.com>
2024-01-02 15:01:39 -0300

    initial commit

---
+++ app/src/main/java/com/limagustavo/mypomofocus/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.limagustavo.mypomofocus
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
